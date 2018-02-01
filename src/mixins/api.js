5a93dc5b5e0ea3e6049dd6937248883faee2089f
Jason Heylon <jasonheylon@gmail.com>
2018-02-02 01:59:04 +0800

    list page and detail page

--- src/mixins/api.js
+++ src/mixins/api.js
@@ -2,7 +2,33 @@ import wepy from 'wepy'
 
 const BASE_URL = 'https://ruby-china.org/api/v3/'
 
-export let queryPopularTopics = (page) => {
+let queryPopularTopics = (page) => {
+  let url = `${BASE_URL}topics.json?type=excellent`
+  return new Promise((resolve, reject) => {
+    wepy.request({
+      url: url
+    }).then((data) => {
+      resolve(data.data)
+    }).catch((error) => {
+      console.error(error)
+    })
+  })
+}
+
+let queryTopicDetail = (topicId) => {
+  let url = `${BASE_URL}topics/${topicId}.json`
+  return new Promise((resolve, reject) => {
+    wepy.request({
+      url: url
+    }).then((data) => {
+      resolve(data.data)
+    }).catch((error) => {
+      console.error(error)
+    })
+  })
+}
+
+let queryTopics = (page) => {
   let url = `${BASE_URL}topics.json`
   return new Promise((resolve, reject) => {
     wepy.request({
@@ -13,9 +39,38 @@ export let queryPopularTopics = (page) => {
       console.error(error)
     })
   })
+}
 
+let queryJobs = (page) => {
+  let url = `${BASE_URL}topics.json?node_id=25`
+  return new Promise((resolve, reject) => {
+    wepy.request({
+      url: url
+    }).then((data) => {
+      resolve(data.data)
+    }).catch((error) => {
+      console.error(error)
+    })
+  })
+}
+
+let queryReplies = (topicId) => {
+  let url = `${BASE_URL}topics/${topicId}/replies.json?`
+  return new Promise((resolve, reject) => {
+    wepy.request({
+      url: url
+    }).then((data) => {
+      resolve(data.data)
+    }).catch((error) => {
+      console.error(error)
+    })
+  })
 }
 
 export default {
-  queryPopularTopics: queryPopularTopics
+  queryPopularTopics: queryPopularTopics,
+  queryTopicDetail: queryTopicDetail,
+  queryTopics: queryTopics,
+  queryJobs: queryJobs,
+  queryReplies: queryReplies
 }
